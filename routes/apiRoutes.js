0f8930b14f56acf6d4f258a1f24b579076a760e6
Melanie Elson <melanie.elson@icloud.com>
2021-04-28 20:10:44 -0600

    update apiroute

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,40 +1,42 @@
+
 const path = require('path');
 const notesData = require('../db/db.json');
 const fs = require('fs');
 const uuid = require("uuid");
 
+
+
 module.exports = (app) => {
-  app.get('/api/notes', (req, res) => {
-      fs.readFile("./db/db.json", "utf8", (err, data) => {
-          if (err) throw err;
-          console.log(data)
-          return res.json(JSON.parse(data))  
-  });
-  app.post('/api/notes', (req, res) => {
-      let newNote = req.body
-      fs.readFile("./db/db.json", "utf8", (err, data) => {
-          if (err) throw err;
-          console.log(newNote)
-          let notes = JSON.parse(data)  
-          newNote.id = uuid.v4() 
-          notes.push(newNote)  
-          fs.writeFile("./db/db.json", JSON.stringify(notes), "utf8", (err, data) => {
-              return res.json(newNote)
-          })
-      })
-  });
-//   app.delete('/api/notes: id', (req, res,) => {
-//       notesData.length = 0;
-//       res.json({
-//           ok: true
-//       });
-//   });
+    app.get('/api/notes', (req, res) => {
+        fs.readFile("./db/db.json", "utf8", (err, data) => {
+            if (err) throw err;
+            console.log(data)
+            return res.json(JSON.parse(data))
+        })
+    });
+    app.post('/api/notes', (req, res) => {
+        let newNote = req.body
+        fs.readFile("./db/db.json", "utf8", (err, data) => {
+            if (err) throw err;
+            console.log(newNote)
+            let notes = JSON.parse(data)
+            newNote.id = uuid.v4()
+            notes.push(newNote)
+            fs.writeFile("./db/db.json", JSON.stringify(notes), "utf8", (err, data) => {
+                return res.json(newNote)
+            })
+        })
+    });
+    app.delete('/api/notes: id', (req, res,) => {
+        fs.readFile("./db/db.json", "utf8", (err, data) => {
+            if (err) throw err;
+            notesData.length = 0;
+            res.json({ ok: true });
+
+        });
+
+    });
 
 
-app.post('/api/notes', (req, res) => {
-    // Empty out the arrays of data
-    notesData.length = 0;
 
-    res.json({ ok: true });
-  });
 };
